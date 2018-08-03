dbb0bc966ec984ed43202b0244aa313c7e322d4a
kali linux <dmoharana4@gmail.com>
2018-08-03 19:31:30 +0530

    commiting for the last time

---
+++ nav_bar.js
@@ -0,0 +1,12 @@
+// // <!--Navigation bar-->
+//
+// $(function(){
+// $("#nav-placeholder").load("nav.html");
+// });
+//
+// // <!--end of Navigation bar-->
+
+
+$(document).ready(function(){
+    $("#nav-placeholder").load("nav.html");
+});
